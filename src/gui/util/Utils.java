960fb064907212db53c36062db2ec6a55847c821
Daniel Pombo <pombo.daniel@gmail.com>
2019-10-02 11:41:13 -0300

    DeparmentForm (dialog) design

---
+++ src/gui/util/Utils.java
@@ -0,0 +1,15 @@
+package gui.util;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class Utils {
+
+	//Palco atual. 
+	//Acessar um Stage aonde o controller que recebeu o evento está.
+	public static Stage currentStage(ActionEvent event) {
+		return (Stage)((Node)event.getSource()).getScene().getWindow();
+		
+	}
+}
